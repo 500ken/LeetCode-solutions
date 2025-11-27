8575941c9d6f1a53c0fffd07c344361802f08dd9
500ken <ken500bear@gmail.com>
2025-11-27 15:29:59 +0800

    add 570

---
+++ 570_Managers_with_at_Least_5_Direct_Reports.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def find_managers(employee: pd.DataFrame) -> pd.DataFrame:
+    counts = employee['managerId'].value_counts()
+    managers = counts[counts >= 5].index
+
+    return employee.loc[
+        employee['id'].isin(managers), 
+        ['name']
+    ].rename(columns={'name': 'name'})
